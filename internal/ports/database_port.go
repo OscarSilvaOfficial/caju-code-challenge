b466c3694c2d6744e6fb9c31c8ca60daae30bcbf
OscarSilvaOfficial <oscarkaka222@gmail.com>
2024-10-28 00:00:27 -0300

    chore: remove comment

--- internal/ports/database_port.go
+++ internal/ports/database_port.go
@@ -2,6 +2,5 @@ package output
 
 type DatabasePort[Data any] interface {
 	Find(collectionOrTable string, where map[string]interface{}) ([]Data, error)
-	// FindById(collectionOrTable string, id interface{}) (*Data, error)
-	// Insert(collectionOrTable string, data Data) (interface{}, error)
+	Insert(collectionOrTable string, data Data) (interface{}, error)
 }
