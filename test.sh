226c8439e7190cc2627f4366b04cc19b8b55229a
OscarSilvaOfficial <oscarkaka222@gmail.com>
2024-10-28 23:14:54 -0300

    chore: add new params to run unittests

--- test.sh
+++ test.sh
@@ -3,14 +3,14 @@
 MONGO_CONTAINER_NAME="caju-code-challenge_db"
 APP_CONTAINER_NAME="caju-code-challenge_api"
 
+echo "Staring containers...\n"
+docker-compose up --build -d
+
+echo "-- Running unit tests --\n"
+docker exec -it $APP_CONTAINER_NAME /usr/local/go/bin/go test ./tests/unit/... -v
+
+echo "-- Running integration tests --\n"
+docker exec -it $APP_CONTAINER_NAME /usr/local/go/bin/go test ./tests/integration/... -count=1 -v
+
 echo "Cleaning...\n"
 docker rm $MONGO_CONTAINER_NAME $APP_CONTAINER_NAME -f || true
-
-docker-compose up --build -d
-sleep 10
-
-echo "\n-- Running unit tests --"
-docker exec -it $APP_CONTAINER_NAME /usr/local/go/bin/go test ./tests/unit/...
-
-echo "-- Running integration tests --"
-docker exec -it $APP_CONTAINER_NAME /usr/local/go/bin/go test ./tests/integration/...
